fa2413611f51b71ac8d4707a0b70c5019dbdbd1f
John Knighlty <jknightly@azknightlys.com>
2017-09-28 10:05:46 -0700

    Update basicCPU.ps1

--- basicCPU.ps1
+++ basicCPU.ps1
@@ -5,7 +5,7 @@ $actionEmail = New-AzureRmAlertRuleEmail -CustomEmail none@none.com
 foreach ($v in $vm)
 {$status = Get-AzureRmVM -ResourceGroupName $v.ResourceGroupName -Name $v.Name -Status
     if ($status.Statuses[1].DisplayStatus -contains 'VM Running'){
-    Add-AzureRmMetricAlertRule -Name vmcpu_lt_25 -Location "West US 2" -ResourceGroup $v.resourcegroupname -TargetResourceId $v.Id -MetricName "Percentage CPU" -Operator LessThan -Threshold 25 -WindowSize 01:00:00 -TimeAggregationOperator Average -Actions $actionEmail -Description "alert on CPU < 25%"
+    Add-AzureRmMetricAlertRule -Name vmcpu_lt_25 -Location $v.location -ResourceGroup $v.resourcegroupname -TargetResourceId $v.Id -MetricName "Percentage CPU" -Operator LessThan -Threshold 25 -WindowSize 01:00:00 -TimeAggregationOperator Average -Actions $actionEmail -Description "alert on CPU < 25%"
     }   
 }
 
