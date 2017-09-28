deab89402f2ac81874fc5691ef26b74cd78d3841
John Knighlty <jknightly@azknightlys.com>
2017-09-28 10:04:08 -0700

    Create basicCPU.ps1
    Monitor for CPU under 25 % for one day - no extensions required.

---
+++ basicCPU.ps1
@@ -0,0 +1,12 @@
+ Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionName 'sub1'
+$vm = Get-AzureRMVm
+$actionEmail = New-AzureRmAlertRuleEmail -CustomEmail none@none.com
+foreach ($v in $vm)
+{$status = Get-AzureRmVM -ResourceGroupName $v.ResourceGroupName -Name $v.Name -Status
+    if ($status.Statuses[1].DisplayStatus -contains 'VM Running'){
+    Add-AzureRmMetricAlertRule -Name vmcpu_lt_25 -Location "West US 2" -ResourceGroup $v.resourcegroupname -TargetResourceId $v.Id -MetricName "Percentage CPU" -Operator LessThan -Threshold 25 -WindowSize 01:00:00 -TimeAggregationOperator Average -Actions $actionEmail -Description "alert on CPU < 25%"
+    }   
+}
+
+ 
